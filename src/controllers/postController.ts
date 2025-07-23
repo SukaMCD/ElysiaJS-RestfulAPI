fbc20eeeff14d38c2385361537f4388854ddd42b
SukaMCD <fabian25march@gmail.com>
2025-07-23 10:46:37 +0700

    InsertData

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -13,3 +13,22 @@ export async function getPosts() {
         console.error(`Error retrieving posts: ${e}`);
     }
 }
+
+//Create a new post
+export async function createPost(option: { title: string; content: string }) {
+    try {
+        const post = await prisma.post.create({
+            data: {
+                title: option.title,
+                content: option.content
+            }
+        });
+        return {
+            success: true,
+            message: "Post created successfully",
+            data: post
+        };
+    } catch (e: unknown) {
+        console.error(`Error creating post: ${e}`);
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,22 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 
-import { getPosts } from '../controllers/PostController';
+import { getPosts, createPost } from '../controllers/PostController';
+ 
+const Routes = new Elysia({ prefix: '/posts' })
 
-const Routes = new Elysia({prefix: '/posts'})
+  .get('/', () => getPosts())
 
-    .get('/', async () => getPosts())
+  .post('/', ({body}) => createPost(body as { title: string; content: string }), {
+    body: t.Object({
+      title: t.String({
+        minLength: 3,
+        maxLength: 100,
+      }),
+      content: t.String({
+        minLength: 3,
+        maxLength: 1000,
+      }),
+    })
+  })
 
 export default Routes;
